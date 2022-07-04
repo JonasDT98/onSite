b933ee5ea3209f4f18466c916b85f25f62aadb2b
JonasDT98 <jonas.del.turco@gmail.com>
2022-07-04 13:43:04 +0200

    Routes
    Made other routes and pages.
    Login page with rout /
    Profile page with rout profile/index
    Home page with rout home/index

--- onSite/routes/web.php
+++ onSite/routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::get('/profile', function(){
     return view('profile/index');
+});
+
+Route::get('/home', function(){
+    return view('home/index');
 });
